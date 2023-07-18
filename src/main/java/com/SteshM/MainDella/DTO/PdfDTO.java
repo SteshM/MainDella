e94facc61735fe673d1593a642016e18d376540e
SteshM <mbithestella0@gmail.com>
2023-07-18 16:03:12 +0300

    Added more classes

---
+++ src/main/java/com/SteshM/MainDella/DTO/PdfDTO.java
@@ -0,0 +1,10 @@
+package com.SteshM.MainDella.DTO;
+
+import lombok.Data;
+
+@Data
+public class PdfDTO {
+    private int pdfId;
+    private String pdfName;
+    private int lessonId;
+}
---
+++ src/main/java/com/SteshM/MainDella/repo/PdfRepo.java
@@ -0,0 +1,8 @@
+package com.SteshM.MainDella.repo;
+
+import com.SteshM.MainDella.Entities.PdfEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PdfRepo extends JpaRepository<PdfEntity , Integer> {
+
+}
